6f8cf3f1dec1075af66c8835de969eb0da503462
Matías Battocchia <matias@riseup.net>
2014-04-15 13:48:03 -0300

    Gasto total en vista editar gasto.

--- public/js/app.js
+++ public/js/app.js
@@ -13,10 +13,23 @@ var dp = $('#fecha').datepicker({
 //   $('input[type=hidden]').val(e.date)
 // });
 
-$( '#pagadores' ).on('change', '.row:last select', function() {
-  selector = $('#pagadores .row:last');
-  selector.clone().insertAfter(selector);
+$( '#pagadores' ).on('change', '.pagos:last select', function() {
+  selector = $('#pagadores .pagos:last');
+  clon = selector.clone();
+	clon.find('input').first().val('');
+	clon.insertAfter(selector);
 });
+
+$( '#pagadores' ).on('change', 'input:enabled', function() {
+	$('#pagadores input:disabled').val( function() {
+		var sum = 0;
+	  $('#pagadores input:enabled').each( function() {
+			sum += Number($(this).val().replace(',','.'));
+		});
+		return String(sum.toFixed(2)).replace('.',',');
+	});
+});
+
 
 $( 'tr' ).click( function() {
   window.location = $(this).data('href');
